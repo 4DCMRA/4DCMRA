231f4dc91fc170a25f26acd7cd3f8a5a4ebde1a6
yuhuachen <chyuhua@seas.upenn.edu>
2015-06-10 17:24:59 -0700

    code update

--- LVSeg/regAtoB.sh
+++ LVSeg/regAtoB.sh
@@ -1,19 +1,28 @@
-TARGETPATH="/home/yuhuachen/WorkingData/UnbiasedTemplate1265/"
-INPUTPATH="/home/yuhuachen/WorkingData/UnbiasedTemplate1253/"
-OUTPUTPATH="/home/yuhuachen/WorkingData/reg1253to1265"
+TARGETPATH="/media/yuhuachen/Document/WorkingData/4DCMRA/LVSegmentation/case_684"
+INPUTPATH="/media/yuhuachen/Document/WorkingData/4DCMRA/LVSegmentation/case_1253"
+OUTPUTPATH="/home/yuhuachen/WorkingData/reg_1253_to_684"
 MASKPATH="/media/yuhuachen/Document/WorkingData/4DCMRA/LVSegmentation/case_1253/Mask/LVMask.nii"
 
 mkdir $OUTPUTPATH
 
-NUMBEROFTHREAD=10
+NUMBEROFTHREAD=8
 TRANSFORMTYPE='s'
-REGISTRATIONFLAG=1
 
-fixedImage=${TARGETPATH}/avg5.nii
+REGISTRATIONFLAG=1
+USINGMASKFLAG=1
+
+fixedImage="${TARGETPATH}/template.nii"
 prefix="${OUTPUTPATH}/reg"
-movingImage="${INPUTPATH}/avg5.nii"
+movingImage="${INPUTPATH}/template.nii"
 # outputImage="${OUTPUTPATH}/phase${p}.nii"
 regCommand="-d 3 -t ${TRANSFORMTYPE} -f ${fixedImage} -m ${movingImage} -o ${prefix} -n ${NUMBEROFTHREAD} "			    
-if [[ ${REGISTRATIONFLAG} -eq 1 ]]; then
-	antsRegistrationSyNPlus.sh $regCommand
+if [[ ${USINGMASKFLAG} -eq 1 ]];then
+	regCommand="${regCommand} -x ${MASKPATH}"
 fi
+
+if [[ ${REGISTRATIONFLAG} -eq 1 ]]; then
+	antsRegistrationSyNQuick.sh $regCommand
+fi
+movingLabel=${INPUTPATH}/segLV.nii
+transCommand="-d 3 --float -f 0 -i ${movingLabel} -o ${OUTPUTPATH}/outputLabel.nii -r ${fixedImage} -n NearestNeighbor  -t ${prefix}1Warp.nii.gz -t ${prefix}0GenericAffine.mat"
+antsApplyTransforms ${transCommand}
